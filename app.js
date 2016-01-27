ab37116341346b23a14aaf0b3b34bdfa2a91b60d
Patrick Powers <patrickpowers10@gmail.com>
2016-01-26 21:48:37 -0500

    Finished mad lib project with form validation and ng-show

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ angular.module("madLib", [])
   $scope.heShe = "he/she";
   $scope.hisHer = "his/her";
   $scope.himHer = "him/her";
+    
+    $scope.showContent = false;
+    $scope.hideContent = true;
+    $scope.showFunction = function() {
+            $scope.showContent = !$scope.showContent;
+            $scope.hideContent = !$scope.hideContent;
+        }
+    
+    $scope.showFunctionAndReset = function() {
+            $scope.showContent = !$scope.showContent;
+            $scope.hideContent = !$scope.hideContent;
+            $scope.mName = "";    
+            $scope.dirtyTasks = "";
+            $scope.obnoxious = "";
+            $scope.jobTitle = "";
+            $scope.celebrity = "";
+            $scope.hugeNumber = "";
+            $scope.tediousTask = "";
+            $scope.uselessSkill = "";
+            $scope.adjective = "";
+        }
 
   $scope.changeGender = function(genderType) {
     if (genderType == 'male'){
